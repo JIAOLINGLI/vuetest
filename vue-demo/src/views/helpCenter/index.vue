c4ab5994b0fe724926bce9ba022976da71f611d3
jolly <534280386@qq.com>
2021-11-19 14:54:35 +0800

    帮助中心

--- vue-demo/src/views/helpCenter/index.vue
+++ vue-demo/src/views/helpCenter/index.vue
@@ -16,25 +16,20 @@
         </p>
        
       </div>
-      <div class="help-body2">
-        <p class="hh4">创建系统角色</p>
-        <div class="help-body2s">
-          <ul class="ing-t20">
-            菜单：系统设置-权限管理-角色管理
-            <li class="ing-352" v-for="(item) in lis" :key="item">{{item}}</li>
-          </ul>
-          <center><img src="@/assets/js1.png" class="js1" /></center>
-          <center><img src="@/assets/js-2.png" class="js2" /></center>
-        </div>
+     
+      <contentbox :title="title"></contentbox>
+
       </div>
-      <div class="help-body3"></div>
     </div>
-  </div>
+
 </template>
 
 <script>
+  import contentbox from './components/content.vue'
 export default {
-  components: {},
+  components: {
+        contentbox
+  },
   data() {
     return {
       list1: [
